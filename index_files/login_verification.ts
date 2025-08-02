5c8268a5ff9e4958f24147b8bb75fe4ac7e7e994
Syed Bilal Alam <syedbilalalam2006@gmail.com>
2025-08-03 02:11:31 +0500

    Update: Typescript new rules added and some modified

--- index_files/login_verification.ts
+++ index_files/login_verification.ts
@@ -1,5 +1,3 @@
-/// <reference path="./sha256.d.ts"/>
-
 const loginLocation: string | undefined = document.getElementById('loginPageLocation')?.dataset.content;
 if(!loginLocation) throw new Error('Some issue with html document!');
 
