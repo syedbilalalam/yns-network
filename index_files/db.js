c57a4afccbcb0476df20230106c1c1558bdc56c0
Syed Bilal Alam <syedbilalalam2006@gmail.com>
2026-01-13 16:33:20 +0500

    Card: Card no 13 added

--- index_files/db.js
+++ index_files/db.js
@@ -6,6 +6,8 @@ const internetSpeed = ['15Mbps', '30Mbps', '50Mbps', '70Mbps'], paymentMethod = 
 // [0, 1400, 0, 1,'3245435345', 1738385036119, 30, false],
 // [0, 1400, 0, 1,'3245435345', 1738385036119, 30, false],
 const userData = [
+    //  Speed,  PaidAmount, Balance,    PaymentMethod,  TransactionId,  ActivationDate, Duration,   UsedWhileExpired
+    [3, 700, 0, 2, false, 1767755589000, 30, 0],
     [3, 700, 0, 2, false, 1765163589000, 30, 0],
     [3, 700, 0, 2, false, 1762571589000, 30, 0],
     [0, 1900, 0, 2, false, 1759950000000, 30, 0],
